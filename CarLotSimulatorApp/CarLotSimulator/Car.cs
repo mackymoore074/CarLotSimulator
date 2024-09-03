aeb1818777d607eddc9613ca9f3a23a942f6bdfc
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-03 12:02:13 -0700

    done

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+        public static List<Car> parkingLot { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(honkNoise);
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,27 +6,6 @@ namespace CarLotSimulator
 {
     class Program
     {
-
-        public class Car
-        {
-            public int Year { get; set; }
-            public string Make { get; set; }
-            public string Model { get; set; }
-            public string EngineNoise { get; set; }
-            public string HonkNoise { get; set; }
-            public bool IsDriveable { get; set; }
-            public static List<Car> parkingLot { get; set; }
-
-            public void MakeEngineNoise(string engineNoise)
-            {
-                Console.WriteLine(engineNoise);
-            }
-
-            public void MakeHonkNoise(string honkNoise)
-            {
-                Console.WriteLine(honkNoise);
-            }
-        }
         static void Main(string[] args)
         {
             //TODO
